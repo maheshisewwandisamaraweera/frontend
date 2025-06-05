96bf41ae2873a57af018709676f71ed0a44bcb48
maheshisewwandisamaraweera <140188548+maheshisewwandisamaraweera@users.noreply.github.com>
2025-06-05 11:46:34 +0530

    remove login and signup

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { CssBaseline, Container } from "@mui/material";
-import Login from "./components/Login";
-import Signup from "./components/SignUp";
 import ServiceProviderAdminProfile from "./components/ServiceProviderAdminProfilePage";
 import ServiceList from "./components/ServiceList";
 import ServiceStaffList from './components/ServiceStaffList';
@@ -14,8 +12,6 @@ const App: React.FC = () => {
       <CssBaseline />
       <Container maxWidth="sm" sx={{ mt: 5 }}>
         <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
           <Route path="/serviceprovideradminprofile" element={<ServiceProviderAdminProfile />} />
           <Route path="/servicelist" element={<ServiceList />} />
           <Route path="/" element={<ServiceStaffList />} />
