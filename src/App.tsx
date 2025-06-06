ee23ed201cd0d5e6331eadae4d9576f32d132143
Dilshan R.P. <pramoth@geveo.com>
2025-06-07 00:48:15 +0530

    resolve conflictS after merging serviceprovideradmin branch

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Link,Navigate } from "react-router-dom";
 import {
-  CssBaseline, Container,
+  CssBaseline,
   AppBar,
   Toolbar,
   Typography,
